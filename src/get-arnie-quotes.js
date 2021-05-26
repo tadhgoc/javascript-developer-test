e1b477bc62f8a89de684f835664a1899fd4079e8
Tadhg O'Callaghan <tocallaghan@nine.com.au>
2021-05-27 09:37:50 +1000

    Implemented getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,16 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  try {
+    const responses = await Promise.all(urls.map(url => httpGet(url)));
+
+    return responses.map(response => response.status === 200
+      ? { 'Arnie Quote': (JSON.parse(response.body)).message }
+      : { 'FAILURE': (JSON.parse(response.body)).message }
+    );
+  } catch (err) {
+    return [{ 'FAILURE': `Probably some generic message, not ${err.message}` }];
+  }
 };
 
 module.exports = {
